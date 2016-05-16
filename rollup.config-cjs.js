e149fc0eab518b6e76175ea20439013fb79c3449
Alloys Mila <mila.alloys@gmail.com>
2016-05-16 10:54:41 +0300

    make rollup bundle different files for cjs and es6
    some projects might might to use require(), while others might need import, so we compile for two different  formats.

--- rollup.config-cjs.js
+++ rollup.config-cjs.js
@@ -2,7 +2,7 @@ import babel from 'rollup-plugin-babel';
 
 export default {
     entry: 'src/eelog.js',
-    // format: 'cjs',
-    // plugins: [babel()],
-    dest: 'lib/eelog.js'
+    format: 'cjs',
+    plugins: [babel()],
+    dest: 'lib/eelog-cjs.js'
 };
