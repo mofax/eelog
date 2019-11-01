6e010887ecbb2347a94934f29094531175b42bf1
Alloys Mila <mila.alloys@gmail.com>
2019-11-01 15:18:26 +0300

    update tests

--- src/__tests__/tools.test.ts
+++ src/__tests__/tools.test.ts
@@ -1,5 +1,5 @@
 import * as os from "os";
-import { logFormat, logLevels } from "../tools";
+import { buildMeta, logFormat, logLevels } from "../tools";
 
 describe("tools", () => {
     it("has the right logLevels", () => {
@@ -21,5 +21,20 @@ describe("tools", () => {
         const expected = "color=red msg=This message name=test pid=1746 timestamp=2019-10-27T10:29:22.279Z";
         const str = logFormat(meta);
         expect(str).toEqual(expected);
+    });
+
+    it("buildMeta", () => {
+        const name = "some name";
+        const message = "this message";
+        const data = {
+            now: new Date().getTime(),
+        };
+
+        const meta = buildMeta(name, message, data);
+        expect(meta).toEqual({
+            ...data,
+            msg: message,
+            name,
+        });
     });
 });
